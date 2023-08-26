b49a5756c4ed53f1a3391c2510254022348f0aa1
Devayan <sadnyd@gmail.com>
2023-08-27 02:25:01 +0530

    updated js and html files

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 let message="inside main"
-
+let workEl=document.getElementById("work-btn")
 let messageEl=document.getElementById("message-el")
 
+function msg(){
+    console.log(message)
+    messageEl.textContent=message
+}
 function work(){
     message="inside work"
+    msg()
 }
 
 function about(){
     message="inside about"
+    msg()
+    
 }
 
 function contact(){
     message="inside contact"
+    msg()
 }
 
 function blogs(){
     message="inside blogs"
+    msg()
 }
-
-messageEl.textContent=message
