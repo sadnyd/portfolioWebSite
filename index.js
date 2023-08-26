6e62cd6628b5055f40dd3eb241e98a8ca098dde3
Devayan <sadnyd@gmail.com>
2023-08-27 01:06:22 +0530

    new updates

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-btn{
-    background-color: white;
-    border: none;
+let message="inside main"
 
+let messageEl=document.getElementById("message-el")
+
+function work(){
+    message="inside work"
 }
+
+function about(){
+    message="inside about"
+}
+
+function contact(){
+    message="inside contact"
+}
+
+function blogs(){
+    message="inside blogs"
+}
+
+messageEl.textContent=message
